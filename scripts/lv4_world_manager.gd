0e4df5662747f23409a81c49cb7f859e5c9067d8
Ahmad Banaag <banaagahmad10@gmail.com>
2026-05-12 22:13:54 +0800

    red vignette added

--- scripts/lv4_world_manager.gd
+++ scripts/lv4_world_manager.gd
@@ -1,8 +1,18 @@
 extends Node
 
 var in_reflection_world = false
+
+var stability = 100.0
+var max_stability = 100.0
+
+var drain_rate = 20.0      # per second in reflection world
+var recover_rate = 10.0    # per second in normal world
+
 var can_switch = true
 var switch_cooldown = 5.0
+
+var drain_enabled = true
+var is_dead = false
 
 
 @onready var normal_world = $"../NormalWorld"
@@ -18,6 +28,26 @@ func _ready():
 	set_world_collision(normal_world, true)
 	set_world_collision(reflection_world, false)
 
+func trigger_break():
+
+	get_tree().reload_current_scene()
+	
+func check_stability():
+
+	if stability <= 0:
+		trigger_break()
+		
+func _process(delta):
+
+	if in_reflection_world and drain_enabled:
+		stability -= drain_rate * delta
+	else:
+		stability += recover_rate * delta
+
+	stability = clamp(stability, 0, max_stability)
+
+	check_stability()
+
 
 func _input(event):
 
@@ -31,8 +61,9 @@ func switch_dimension():
 	#Shaking of Camera
 	$"../CameraLv_4".apply_shake()
 	#For playing transition
+	
 	$"../CanvasLayer/DimensionTransition".play("dimension_transition")
-
+	
 	in_reflection_world = !in_reflection_world
 
 	normal_world.visible = !in_reflection_world
@@ -41,11 +72,21 @@ func switch_dimension():
 	set_world_collision(normal_world, !in_reflection_world)
 	set_world_collision(reflection_world, in_reflection_world)
 	
+	var anim = $"../CanvasLayer/AnimationPlayer"
+	var death_anim = $"../CanvasLayer/AnimationPlayer2"
+	
+	drain_enabled = false
+	anim.play("cooldown_start")
+	
 	#Cooldown	
 	await get_tree().create_timer(switch_cooldown).timeout
-
+	anim.play("cooldown_end")
+	drain_enabled = true
+	
+	death_anim.play("red_vignette_in")
 	can_switch = true
-
+	
+	
 
 func set_world_collision(world_node, enable):
 
